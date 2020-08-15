5782b5368636e1e07ef776329033dd8dca05245f
swachith reddy <swachith12@gmail.com>
2020-08-15 14:56:17 +0530

    upload

---
+++ src/test/java/love/testy.java
@@ -0,0 +1,24 @@
+package love;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class testy {
+
+	thea h=new thea();
+
+	@Test
+	public void testingShrek() {
+		assertEquals(h.th(),40);
+		
+		
+	}
+	@Test
+	public void testingstuart() {
+		assertEquals(h.th(),100);
+	}
+
+}
